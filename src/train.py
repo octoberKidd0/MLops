97287c2fd95861e8b7b36e4757da2a06b119da4d
octoberKidd0 <victoriamonow@gmail.com>
2024-12-11 12:10:30 +0800

    changing the paths

--- src/train.py
+++ src/train.py
@@ -7,7 +7,7 @@ def train_model(params):
     img_size = params["train"]["img_size"]
     
     model = YOLO(model_arch)
-    model.train(data="data.yaml", epochs=epochs, imgsz=img_size)
+    model.train(data="C:\Users\Виктория\Desktop\MLops\MLops\data.yaml", epochs=epochs, imgsz=img_size)
 
 if __name__ == "__main__":
     with open("params.yaml") as f:
